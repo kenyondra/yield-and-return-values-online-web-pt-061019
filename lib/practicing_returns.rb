592fd58a25338f948e88db704fbe359f9e62e981
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-23 15:24:57 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -10,5 +10,9 @@ def hello(array)
   collection
 end
 
+<<<<<<< HEAD
 
+=======
+binding.pry
+>>>>>>> b75c20b4b034f31e7464faaf0cee71c7cecdfa03
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
